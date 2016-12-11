b37ade11f49556b630392159127f868aae7d821a
JIukaviy <dobryniny07@mail.ru>
2016-12-12 02:24:24 +1000

    Add debug logging in FlashlightPeriphery

--- MATE_ROV_2017_DNO/FlashlightPeriphery.h
+++ MATE_ROV_2017_DNO/FlashlightPeriphery.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "Arduino.h"
+
 class FlashlightPeriphery_t {
 private:
 	int _pin;
@@ -7,15 +9,22 @@ private:
 public:
 	FlashlightPeriphery_t(int pin) {
 		_pin = pin;
+		_state = false;
 		pinMode(_pin, OUTPUT);
 	}
 
 	void TurnOn() {
+#ifdef _DEBUG
+		Serial.println("Turning ON flashlight");
+#endif
 		digitalWrite(_pin, HIGH);
 		_state = true;
 	}
 
 	void TurnOff() {
+#ifdef _DEBUG
+		Serial.println("Turning OFF flashlight");
+#endif
 		digitalWrite(_pin, LOW);
 		_state = false;
 	}
