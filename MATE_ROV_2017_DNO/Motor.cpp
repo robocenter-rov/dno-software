8ac012f8e32919cf4af6aef680c7790e2b8ba8be
J-Veber <kovalsky.corp@gmail.com>
2016-12-12 18:52:43 +1000

    Add PololuMotor class

--- MATE_ROV_2017_DNO/Motor.cpp
+++ MATE_ROV_2017_DNO/Motor.cpp
@@ -1,9 +1,27 @@
 #include "Motor.h"
 
-
-
 Motor_t::Motor_t() {}
 
 void Motor_t::SetThrust(float thrust) {}
 
 Motor_t::~Motor_t() {}
+
+PololuMotor_t::PololuMotor_t(unsigned char INA, unsigned char INB) : _md(INA, INB, 0, 0, 0, 0, 0, 0) { 
+#ifdef _DEBUG
+	if (_md.getM1Fault()) {
+		Serial.println("Failed initialize motor");
+	} else {
+		Serial.println("Successfully initialize motor");
+	}
+	delay(200);
+#endif
+}
+
+void PololuMotor_t::SetThrust(float thrust) {
+#ifdef _DEBUG
+	Serial.print("Set thrust");
+	Serial.print(thrust);
+	delay(200);
+#endif
+	_md.setM1Speed(400 * thrust);
+}
--- MATE_ROV_2017_DNO/Motor.h
+++ MATE_ROV_2017_DNO/Motor.h
@@ -1,10 +1,21 @@
 #pragma once
+
+#include <DualVNH5019MotorShield.h>
+
 class Motor_t {
 public:
 	Motor_t();
 	
-	void SetThrust(float thrust);
+	virtual void SetThrust(float thrust);
 
-	~Motor_t();
+	virtual ~Motor_t();
 };
 
+class PololuMotor_t : public Motor_t {
+private:
+	DualVNH5019MotorShield _md;
+public:
+	PololuMotor_t(unsigned char INA, unsigned char INB);
+
+	void SetThrust(float thrust) override;
+};
