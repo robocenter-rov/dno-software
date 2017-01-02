132b8d8f734a9d8d13dafff7015416e68d6876e7
JIukaviy <dobryniny07@mail.ru>
2017-01-03 00:28:11 +1000

    Make Task::UpdateState function pure virtual

--- MATE_ROV_2017_DNO/Task.h
+++ MATE_ROV_2017_DNO/Task.h
@@ -13,7 +13,7 @@ public:
 	Task_t(unsigned int id) : _id(id), _worker_id(0) {}
 	virtual ~Task_t() {}
 	virtual bool LockNeededResources(RESOURCE& out_locked_resource) { return true; }
-	virtual bool UpdateState(SelfExpandoContainer_t<TaskState_t>& out_state) { return true; }
+	virtual bool UpdateState(SelfExpandoContainer_t<TaskState_t>& out_state) = 0;
 	unsigned int GetId() const { return _id; }
 	void SetWorkerId(unsigned int worker_id);
 };
