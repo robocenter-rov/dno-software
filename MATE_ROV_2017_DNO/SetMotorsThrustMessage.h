2e9d77c0dd1ed2229398ff93fa97224059e6c4bc
JIukaviy <dobryniny07@mail.ru>
2017-01-25 17:25:13 +1000

    Change type of motorThrust in SetMotorThrustMessage from unsigned int to int

--- MATE_ROV_2017_DNO/SetMotorsThrustMessage.h
+++ MATE_ROV_2017_DNO/SetMotorsThrustMessage.h
@@ -2,11 +2,11 @@ reg_msg_info(1, SetMotorsThrust)
 
 reg_msg_begin_fields_list
 
-reg_msg_field(unsigned int, motor1Thrust)
-reg_msg_field(unsigned int, motor2Thrust)
-reg_msg_field(unsigned int, motor3Thrust)
-reg_msg_field(unsigned int, motor4Thrust)
-reg_msg_field(unsigned int, motor5Thrust)
-reg_msg_last_field(unsigned int, motor6Thrust)
+reg_msg_field(int, motor1Thrust)
+reg_msg_field(int, motor2Thrust)
+reg_msg_field(int, motor3Thrust)
+reg_msg_field(int, motor4Thrust)
+reg_msg_field(int, motor5Thrust)
+reg_msg_last_field(int, motor6Thrust)
 
 reg_msg_end_fields_list
