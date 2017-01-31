33db1e857e4d84c197bfbccefa91501414c9878c
JIukaviy <dobryniny07@mail.ru>
2017-02-01 00:04:05 +1000

    Code style fix on SensorManager

--- MATE_ROV_2017_DNO/SensorManager.cpp
+++ MATE_ROV_2017_DNO/SensorManager.cpp
@@ -1,13 +1,14 @@
 #include "SensorManager.h"
 
-SensorManager_t::SensorManager_t() {
-
+SensorManager_t::SensorManager_t(SensorRotation_t* sensor_rotation, SensorDepth_t* sensor_depth) {
+	_sensorRotation = sensor_rotation;
+	_sensorDepth = sensor_depth;
 }
 
 float SensorManager_t::GetDepth() const {
-	return SensorDepth->GetDepth();
+	return _sensorDepth->GetDepth();
 }
 
 SensorRotation_t::Data_t SensorManager_t::GetRotation() const {
-	return SensorRotation->GetRotation();
+	return _sensorRotation->GetRotation();
 }
--- MATE_ROV_2017_DNO/SensorManager.h
+++ MATE_ROV_2017_DNO/SensorManager.h
@@ -1,13 +1,14 @@
 #pragma once
+#include "SensorData.h"
 #include "SensorDepth.h"
 #include "SensorRotation.h"
 
 class SensorManager_t {
 private:
-	SensorDepth_t* SensorDepth;
-	SensorRotation_t* SensorRotation;
+	SensorRotation_t* _sensorRotation;
+	SensorDepth_t* _sensorDepth;
 public:
-	SensorManager_t();
+	SensorManager_t(SensorRotation_t* sensor_rotation, SensorDepth_t* sensor_depth);
 
 	float GetDepth() const;
 	SensorRotation_t::Data_t GetRotation() const;
