68a266a4dfef0702e4e29d5b9717b01487f7ba4b
JIukaviy <dobryniny07@mail.ru>
2017-02-01 00:00:53 +1000

    Register ManipulatorPeriphery in PeripheryManager

--- MATE_ROV_2017_DNO/PeripheryManager.h
+++ MATE_ROV_2017_DNO/PeripheryManager.h
@@ -1,13 +1,16 @@
 #pragma once
 
 #include "FlashlightPeriphery.h"
+#include "ManipulatorPeriphery.h"
 
 class PeripheryManager_t {
 private:
 	FlashlightPeriphery_t* _flashlight;
+	ManipulatorPeriphery_t* _manipulator;
 public:
-	PeripheryManager_t(FlashlightPeriphery_t* flashlight) {
+	PeripheryManager_t(FlashlightPeriphery_t* flashlight, ManipulatorPeriphery_t* manipulator) {
 		_flashlight = flashlight;
+		_manipulator = manipulator;
 	}
 
 	void TurnOnFlashlight() const {
@@ -24,5 +27,9 @@ public:
 
 	bool GetFlashlightState() const {
 		return _flashlight->GetState();
+	}
+
+	ManipulatorPeriphery_t* GetManipulator() const {
+		return _manipulator;
 	}
 };
