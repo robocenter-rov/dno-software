e8871ba5c46869289d36e7973fb9eeaee67dffc5
JIukaviy <dobryniny07@mail.ru>
2017-02-01 00:05:38 +1000

    Add SendSensorData task

---
+++ MATE_ROV_2017_DNO/SendSensorDataTask.cpp
@@ -0,0 +1,23 @@
+#include "SendSensorDataTask.h"
+
+SendSensorDataTask_t::SendSensorDataTask_t(int id, SensorManager_t* sensor_manager, Communicator_t* communicator, unsigned int interval) : Task_t(id) {
+	_sensor_manager = sensor_manager;
+	_communicator = communicator;
+	_interval = interval;
+	_last_send_time = 0;
+}
+
+bool SendSensorDataTask_t::LockNeededResources(RESOURCE& out_locked_resource) {
+	return true;
+}
+
+bool SendSensorDataTask_t::UpdateState(SelfExpandoContainer_t<TaskState_t>& out_state) {
+	if (millis() - _last_send_time > _interval) {
+		SensorData_t t = _sensor_manager->GetSensorData();
+		_communicator->SendSensorData(&t);
+		out_state = SendSensorDataTaskState_t(_id, _worker_id);
+		_last_send_time = millis();
+	}
+
+	return false;
+}
---
+++ MATE_ROV_2017_DNO/SendSensorDataTask.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "Task.h"
+#include "SensorManager.h"
+#include "Communicator.h"
+
+class SendSensorDataTask_t : public Task_t {
+private:
+	SensorManager_t* _sensor_manager;
+	Communicator_t* _communicator;
+	unsigned int _interval;
+
+	unsigned int _last_send_time;
+public:
+	SendSensorDataTask_t(int id, SensorManager_t* sensor_manager, Communicator_t* communicator, unsigned int interval);
+	bool LockNeededResources(RESOURCE& out_locked_resource) override;
+	bool UpdateState(SelfExpandoContainer_t<TaskState_t>& out_state) override;
+};
