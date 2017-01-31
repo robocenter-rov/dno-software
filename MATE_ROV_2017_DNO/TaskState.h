ba5415e027d025f979250634ae2f75036b44464e
JIukaviy <dobryniny07@mail.ru>
2017-02-01 00:07:04 +1000

    Add TaskStates

--- MATE_ROV_2017_DNO/TaskState.h
+++ MATE_ROV_2017_DNO/TaskState.h
@@ -8,6 +8,10 @@ enum TASK_STATUS {
 	TS_CANCELLED,
 
 	TS_BLINKING,
+	TS_SENDING,
+
+	TS_BLUETOOTH_WAITING_FOR_CONNECTION,
+	TS_BLUETOOTH_DATA_READED
 };
 
 struct TaskState_t {
@@ -62,6 +66,38 @@ struct BlinkFlashlightTaskState_t : public TaskState_t {
 		buffer.worker_id = worker_id;
 		buffer.status = status;
 		buffer.blinked_count = blinked_count;
+
+		return ByteArray_t(buffer);
+	}
+};
+
+struct SendSensorDataTaskState_t : public TaskState_t {
+	SendSensorDataTaskState_t(unsigned int task_id, unsigned int worker_id) : TaskState_t(task_id, worker_id, TS_BLINKING) {}
+};
+
+struct BluetoothWaitingForConnectionTaskState_t : public TaskState_t {
+	BluetoothWaitingForConnectionTaskState_t(unsigned int task_id, unsigned int worker_id) : TaskState_t(task_id, worker_id, TS_BLUETOOTH_WAITING_FOR_CONNECTION) {}
+};
+
+struct BluetoothDataReadedTaskState_t : public TaskState_t {
+	char message[7];
+	BluetoothDataReadedTaskState_t(unsigned int task_id, unsigned int worker_id, char* msg) : TaskState_t(task_id, worker_id, TS_BLUETOOTH_DATA_READED) {
+		memcpy(message, msg, 7);
+	}
+	ByteArray_t ToByteArray() const override {
+#pragma pack(push, 1)
+		struct {
+			unsigned int task_id;
+			unsigned int worker_id;
+			TASK_STATUS status;
+			char message[7];
+		} buffer;
+#pragma pack(pop)
+
+		buffer.task_id = task_id;
+		buffer.worker_id = worker_id;
+		buffer.status = status;
+		memcpy(buffer.message, message, 7);
 
 		return ByteArray_t(buffer);
 	}
