bd654203ac1dab3ab628b19b31a4607a5eab0fc8
JIukaviy <dobryniny07@mail.ru>
2017-01-31 23:53:38 +1000

    Make functions of SensorDepth are virtual

--- MATE_ROV_2017_DNO/SensorDepth.h
+++ MATE_ROV_2017_DNO/SensorDepth.h
@@ -3,8 +3,9 @@
 class SensorDepth_t {
 private:
 public:
+	virtual ~SensorDepth_t() {}
 	SensorDepth_t();
 
-	float GetDepth();
-	float GetRawData();
+	virtual float GetDepth() = 0;
+	virtual float GetRawData() = 0;
 };
