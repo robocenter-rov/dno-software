ecdcb8aaa01bc6d9df0ac8737e9dfe109b2d8234
JIukaviy <dobryniny07@mail.ru>
2017-02-02 11:40:11 +1000

    Add new exceptions

--- MATE_ROV_2017_DNO/RegisterExceptions.h
+++ MATE_ROV_2017_DNO/RegisterExceptions.h
@@ -19,3 +19,11 @@ reg_except(203, SC_RECEIVER_NOT_SETTED, "Receiver function is not setted")
 reg_except(300, TP_FULL, "Can't add task to pool: pool is full")
 reg_except(301, TP_RESOURCE_LOCKED, "Can't add task to pool: resource locked")
 reg_except(302, TP_INVALID_WORKER_ID, "Invalid worker id")
+
+//Tasks
+
+reg_except(400, T_BLUETOOTH_MESSAGE_TOO_BIG, "Bluetooth message is bigger then buffer")
+
+//StdMain
+
+reg_except(500, SM_WRONG_WORKER_ID, "Wrong worker id")
