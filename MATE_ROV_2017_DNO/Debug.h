38d447ffb1e53c2943b4a652dee56a67ad4454e6
JIukaviy <dobryniny07@mail.ru>
2017-02-07 18:15:26 +1000

    Add LOGHEX macros in Dubeug

--- MATE_ROV_2017_DNO/Debug.h
+++ MATE_ROV_2017_DNO/Debug.h
@@ -2,7 +2,8 @@
 
 #ifdef _DEBUG
 #define LOG(str) Serial.print(str);
-#define LOGLN(str) Serial.println(str);
+#define LOGHEX(str) Serial.print(str, HEX);
+#define LOGLN(str) Serial.println(str); delay(50);
 #else
 #define LOG(str)
 #define LOGLN(str)
