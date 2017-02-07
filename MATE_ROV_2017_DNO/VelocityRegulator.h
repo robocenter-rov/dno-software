d1d775ed9f944ff8c920e287ae9a1f062d77e613
JIukaviy <dobryniny07@mail.ru>
2017-02-07 18:23:40 +1000

    Fix VelocityRegulator

--- MATE_ROV_2017_DNO/VelocityRegulator.h
+++ MATE_ROV_2017_DNO/VelocityRegulator.h
@@ -15,6 +15,9 @@ class AutoVelocity_t {
 	double _target_x_velocity;
 	double _target_z_velocity;
 	double _target_y_velocity;
+
+	double _last_depth;
+	unsigned int _last_micros;
 public:
 	AutoVelocity_t(MotorsForceDistributor_t* force_distrib, SensorManager_t* sensor_manager,
 		PidCoefficient_t pid_coeff) : _pid(&_pid_input, &_pid_output, &_target_z_velocity, pid_coeff.D, pid_coeff.I, pid_coeff.D, DIRECT) 
@@ -27,9 +30,13 @@ public:
 	}
 
 	void Update() {
-		_pid_input = _sensor_manager->GetDepth();
+		float current_depth = _sensor_manager->GetDepth() - _last_depth;
+		unsigned int current_micros = micros();
+		_pid_input = (_sensor_manager->GetDepth() - current_depth) / ((current_depth - _last_micros) / 1000.f);
 		_pid.Compute();
 		_force_distrib->AddLocalMoveForce(_target_x_velocity, _target_y_velocity, _pid_output);
+		_last_depth = current_depth;
+		_last_micros = current_micros;
 	}
 
 	void SetVelocity(float x, float y, float z) {
