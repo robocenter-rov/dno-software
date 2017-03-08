d6889b2c8afa50a478d05ce67f13776018d40311
J-Veber <kovalsky.corp@gmail.com>
2017-03-08 12:07:24 +1000

    Add Awake() and Destroy() methods

--- MATE_ROV_2017_DNO/Task.h
+++ MATE_ROV_2017_DNO/Task.h
@@ -11,6 +11,8 @@ protected:
 public:
 	Task_t(unsigned int tag) : _tag(tag) {}
 	virtual ~Task_t() {}
+	virtual bool Awake() {}
+	virtual bool Destroy() {}
 	virtual bool LockNeededResources(RESOURCE& out_locked_resource, int worker_id) { return true; }
 	virtual bool UpdateState(SelfExpandoContainer_t<TaskState_t>& out_state) = 0;
 	unsigned int GetTag() const { return _tag; }
