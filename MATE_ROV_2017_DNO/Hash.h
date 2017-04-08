75c0bbfebcad0c48166dba233b045ca078907d24
Lukaviy <jiukaviy@gmail.com>
2017-04-08 23:23:58 +1000

    Hash: Add some helper function

--- MATE_ROV_2017_DNO/Hash.h
+++ MATE_ROV_2017_DNO/Hash.h
@@ -1,5 +1,20 @@
 #pragma once
 
-inline uint32_t HashLy(char byte, uint32_t hash) {
+inline uint32_t HashLy(char byte, uint32_t hash = 0) {
 	return (hash * 1664525) + byte + 1013904223;
+}
+
+inline uint32_t HashLy(void* buffer, int size, uint32_t hash = 0) {
+	auto b = reinterpret_cast<char*>(buffer);
+
+	for (int i = 0; i < size; i++) {
+		hash = HashLy(b[i], hash);
+	}
+
+	return hash;
+}
+
+template<typename T>
+uint32_t HashLy(T val, uint32_t hash = 0) {
+	return HashLy(&val, sizeof(T), hash);
 }
