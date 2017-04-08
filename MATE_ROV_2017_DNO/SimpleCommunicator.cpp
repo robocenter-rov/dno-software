47b2312c6b535334e5ba198c8aaaa8c565dba1d2
Lukaviy <jiukaviy@gmail.com>
2017-04-08 23:24:43 +1000

    SimpleCommunicator: Use callbacks to retrieve send data

--- MATE_ROV_2017_DNO/SimpleCommunicator.cpp
+++ MATE_ROV_2017_DNO/SimpleCommunicator.cpp
@@ -1,5 +1,6 @@
 #include "SimpleCommunicator.h"
 #include "DataReader.h"
+#include "Hash.h"
 
 enum SEND_BLOCK_IDS {
 	SBI_STATE = 0,
@@ -21,6 +22,18 @@ enum RECEIVE_BLOCK_IDS {
 
 SimpleCommunicator_t::SimpleCommunicator_t(ConnectionProvider_t* connection_provider) {
 	_connection_provider = connection_provider;
+}
+
+void SimpleCommunicator_t::Begin() {
+	
+}
+
+void SimpleCommunicator_t::Stop() {
+	
+}
+
+bool SimpleCommunicator_t::Connected() const {
+	return millis() - _last_msg_receive_time < _receive_time_out;
 }
 
 int SimpleCommunicator_t::Update() {
@@ -49,18 +62,12 @@ int SimpleCommunicator_t::Update() {
 			switch (block_id) {
 				case SBI_STATE:
 					struct {
-						struct {
-							int flashlight_state : 1;
-							int read_bluetooth : 1;
-							int send_raw_sensor_data : 1;
-							int send_calibrated_sensor_data : 1;
-							int send_pid_state : 1;
-							int send_motors_state : 1;
-							int rest : 2;
-						} state;
+						State_t state;
 						uint8_t last_i2c_scan;
 					} state;
 					dr.Read(state);
+					_all_state = state.state;
+					_last_received_i2c_scan_token = state.last_i2c_scan;
 					_on_state_receive.callback(
 						_on_state_receive.data, 
 						state.state.flashlight_state, 
@@ -136,21 +143,39 @@ int SimpleCommunicator_t::Update() {
 					);
 				break;
 				case RBI_PID:
-					Pids_t pids;
+					struct {
+						float depth_p;
+						float depth_i;
+						float depth_d;
+
+						float yaw_p;
+						float yaw_i;
+						float yaw_d;
+
+						float pitch_p;
+						float pitch_i;
+						float pitch_d;
+					} pids;
 					dr.Read(pids);
-					_on_pid_receive.callback(_on_pid_receive.data,
-						pids.depth_p,
-						pids.depth_i,
-						pids.depth_d,
 
-						pids.yaw_p,
-						pids.yaw_i,
-						pids.yaw_d,
+					uint32_t pids_hash = HashLy(pids);
+					if (_pids_hash != pids_hash) {
+						_on_pid_receive.callback(_on_pid_receive.data,
+							pids.depth_p,
+							pids.depth_i,
+							pids.depth_d,
 
-						pids.pitch_p,
-						pids.pitch_i,
-						pids.pitch_d
-					);
+							pids.yaw_p,
+							pids.yaw_i,
+							pids.yaw_d,
+
+							pids.pitch_p,
+							pids.pitch_i,
+							pids.pitch_d
+						);
+					}
+
+					_pids_hash = pids_hash;
 				break;
 				default: ;
 			}
@@ -162,121 +187,206 @@ int SimpleCommunicator_t::Update() {
 		_connection_provider->Write(_last_sended_msg_id);
 
 		_connection_provider->Write(SBI_STATE);
-		_connection_provider->Write(_all_state);
-		_connection_provider->Write(_scanned_i2c_devices);
+
+		bool flashlight_state;
+		_on_state_need.callback(_on_state_need.data, flashlight_state);
+		struct {
+			int flashlight_state : 1;
+		} state {flashlight_state};
+
+		_connection_provider->Write(state);
+
+		bool scanned; bool pca1; bool pca2; bool hmc58x3; bool itg3200; bool adxl345; bool bmp085; bool ms5803;
+		_on_scanned_i2c_devices_need.callback(_on_scanned_i2c_devices_need.data,
+			scanned, pca1, pca2, hmc58x3, itg3200, adxl345, bmp085, ms5803);
+
+		struct {
+			int pca1 : 1;
+			int pca2 : 1;
+			int hmc58x3 : 1;
+			int itg3200 : 1;
+			int adxl345 : 1;
+			int bmp085 : 1;
+			int ms5803 : 1;
+		} scanned_i2c_devices{ pca1, pca2, hmc58x3, itg3200, adxl345, bmp085, ms5803 };
+
+		if (scanned) {
+			_last_i2c_scan_token = _last_received_i2c_scan_token;
+		}
+
 		_connection_provider->Write(_last_i2c_scan_token);
+		_connection_provider->Write(scanned_i2c_devices);
 
 		_connection_provider->Write(SBI_SENSOR_DATA);
-		_connection_provider->Write(_sensor_data);
+
+		struct {
+			float q1;
+			float q2;
+			float q3;
+			float q4;
+			float depth;
+		} sensor_data;
+		_on_sensor_data_need.callback(_on_sensor_data_need.data,
+			sensor_data.q1, sensor_data.q2, sensor_data.q3, sensor_data.q4, sensor_data.depth
+		);
+
+		_connection_provider->Write(sensor_data);
 
 		if (_all_state.send_raw_sensor_data) {
 			_connection_provider->Write(SBI_RAW_SENSOR_DATA);
-			_connection_provider->Write(_raw_sensor_data);
+
+			struct {
+				int ax;
+				int ay;
+				int az;
+
+				int mx;
+				int my;
+				int mz;
+
+				int gx;
+				int gy;
+				int gz;
+
+				float depth;
+			} raw_sensor_data;
+
+			_on_raw_sensor_data_need.callback(_on_raw_sensor_data_need.data,
+				raw_sensor_data.ax, raw_sensor_data.ay, raw_sensor_data.az,
+				raw_sensor_data.mx, raw_sensor_data.my, raw_sensor_data.mz,
+				raw_sensor_data.gx, raw_sensor_data.gy, raw_sensor_data.gz,
+				raw_sensor_data.depth
+			);
+
+			_connection_provider->Write(raw_sensor_data);
 		}
 
 		if (_all_state.send_calibrated_sensor_data) {
 			_connection_provider->Write(SBI_CALIBRATED_SENSOR_DATA);
-			_connection_provider->Write(_calibrated_sensor_data);
+
+			struct {
+				float ax;
+				float ay;
+				float az;
+
+				float mx;
+				float my;
+				float mz;
+
+				float gx;
+				float gy;
+				float gz;
+
+				float depth;
+			} calibrated_sensor_data;
+
+			_on_calibrated_sensor_data_need.callback(_on_calibrated_sensor_data_need.data,
+				calibrated_sensor_data.ax, calibrated_sensor_data.ay, calibrated_sensor_data.az,
+				calibrated_sensor_data.mx, calibrated_sensor_data.my, calibrated_sensor_data.mz,
+				calibrated_sensor_data.gx, calibrated_sensor_data.gy, calibrated_sensor_data.gz,
+				calibrated_sensor_data.depth
+			);
+
+			_connection_provider->Write(calibrated_sensor_data);
 		}
 
 		if (_all_state.send_pid_state) {
 			_connection_provider->Write(SBI_PID_STATE_RECEIVE);
-			_connection_provider->Write(_pids_state);
+
+			struct {
+				float depth_in;
+				float depth_target;
+				float depth_out;
+
+				float yaw_in;
+				float yaw_target;
+				float yaw_out;
+
+				float pitch_in;
+				float pitch_target;
+				float pitch_out;
+			} pids_state;
+
+			_on_pids_state_need.callback(_on_pids_state_need.data,
+				pids_state.depth_in, pids_state.depth_target, pids_state.depth_out,
+				pids_state.yaw_in, pids_state.yaw_target, pids_state.yaw_out,
+				pids_state.pitch_in, pids_state.pitch_target, pids_state.pitch_out
+			);
+
+			_connection_provider->Write(pids_state);
 		}
 
 		if (_all_state.send_motors_state) {
 			_connection_provider->Write(SBI_MOTORS_STATE_RECEIVE);
-			_connection_provider->Write(_motors_state);
+
+			struct {
+				float m1;
+				float m2;
+				float m3;
+				float m4;
+				float m5;
+				float m6;
+			} motors_state;
+
+			_on_motors_state_need.callback(_on_motors_state_need.data,
+				motors_state.m1, motors_state.m2, motors_state.m3, motors_state.m4, motors_state.m5, motors_state.m6
+			);
+
+			_connection_provider->Write(motors_state);
 		}
 
 		if (_all_state.read_bluetooth) {
 			_connection_provider->Write(SBI_BLUETOOTH_MSG_RECEIVE);
-			_connection_provider->Write(_bluetooth_message, 7);
+
+			char* bluetooth_message;
+
+			_on_bluetooth_msg_need.callback(_on_bluetooth_msg_need.data, bluetooth_message);
+
+			_connection_provider->Write(bluetooth_message, 7);
 		}
 	}
 
 	return 0;
 }
 
-void SimpleCommunicator_t::SetState(bool flashlight_state) {
-	_all_state.flashlight_state = flashlight_state;
+void SimpleCommunicator_t::OnStateNeed(void(* callback)(void* data, bool& flashlight_state), void* data) {
+	_on_state_need.callback = callback;
+	_on_state_need.data = data;
 }
 
-void SimpleCommunicator_t::SetSensorData(float q1, float q2, float q3, float q4, float depth) {
-	_sensor_data.q1 = q1;
-	_sensor_data.q2 = q2;
-	_sensor_data.q3 = q3;
-	_sensor_data.q4 = q4;
-	_sensor_data.depth = depth;
+void SimpleCommunicator_t::OnSensorDataNeed(void(* callback)(void* data, float& q1, float& q2, float& q3, float& q4, float& depth), void* data) {
+	_on_sensor_data_need.callback = callback;
+	_on_sensor_data_need.data = data;
 }
 
-void SimpleCommunicator_t::SetRawSensorData(int ax, int ay, int az, int gx, int gy, int gz, int mx, int my, int mz, float depth) {
-	_raw_sensor_data.ax = ax;
-	_raw_sensor_data.ay = ay;
-	_raw_sensor_data.az = az;
-
-	_raw_sensor_data.gx = gx;
-	_raw_sensor_data.gy = gy;
-	_raw_sensor_data.gz = gz;
-
-	_raw_sensor_data.mx = mx;
-	_raw_sensor_data.my = my;
-	_raw_sensor_data.mz = mz;
-
-	_raw_sensor_data.depth = depth;
+void SimpleCommunicator_t::OnRawSensorDataNeed(void(* callback)(void* data, int& ax, int& ay, int& az, int& gx, int& gy, int& gz, int& mx, int& my, int& mz, float& depth), void* data) {
+	_on_raw_sensor_data_need.callback = callback;
+	_on_raw_sensor_data_need.data = data;
 }
 
-void SimpleCommunicator_t::SetCalibratedSensorData(float ax, float ay, float az, float gx, float gy, float gz, float mx, float my, float mz) {
-	_calibrated_sensor_data.ax = ax;
-	_calibrated_sensor_data.ay = ay;
-	_calibrated_sensor_data.az = az;
-
-	_calibrated_sensor_data.gx = gx;
-	_calibrated_sensor_data.gy = gy;
-	_calibrated_sensor_data.gz = gz;
-	
-	_calibrated_sensor_data.mx = mx;
-	_calibrated_sensor_data.my = my;
-	_calibrated_sensor_data.mz = mz;
+void SimpleCommunicator_t::OnCalibratedSensorDataNeed(void(* callback)(void* data, float& ax, float& ay, float& az, float& gx, float& gy, float& gz, float& mx, float& my, float& mz, float& depth), void* data) {
+	_on_calibrated_sensor_data_need.callback = callback;
+	_on_calibrated_sensor_data_need.data = data;
 }
 
-void SimpleCommunicator_t::SetBluetoothMessage(char* bluetooth_message) {
-	memcpy(_bluetooth_message, bluetooth_message, 7);
+void SimpleCommunicator_t::OnBluetoothMessageNeed(void(* callback)(void* data, char*& bluetooth_message), void* data) {
+	_on_bluetooth_msg_need.callback = callback;
+	_on_bluetooth_msg_need.data = data;
 }
 
-void SimpleCommunicator_t::SetPids(float depth_p, float depth_i, float depth_d, float yaw_p, float yaw_i, float yaw_d, float pitch_p, float pitch_i, float pitch_d) {
-	_pids.depth_p = depth_p;
-	_pids.depth_i = depth_i;
-	_pids.depth_d = depth_d;
-
-	_pids.yaw_p = yaw_p;
-	_pids.yaw_i = yaw_i;
-	_pids.yaw_d = yaw_d;
-
-	_pids.pitch_p = pitch_p;
-	_pids.pitch_i = pitch_i;
-	_pids.pitch_d = pitch_d;
+void SimpleCommunicator_t::OnPidsStateNeed(void(* callback)(void* data, float& depth_in, float& depth_target, float& depth_out, float& yaw_in, float& yaw_target, float& yaw_out, float& pitch_in, float& pitch_out, float& pitch_target), void* data) {
+	_on_pids_state_need.callback = callback;
+	_on_pids_state_need.data = data;
 }
 
-void SimpleCommunicator_t::SetMotorsState(float m1, float m2, float m3, float m4, float m5, float m6) {
-	_motors_state.m1 = m1;
-	_motors_state.m2 = m2;
-	_motors_state.m3 = m3;
-	_motors_state.m4 = m4;
-	_motors_state.m5 = m5;
-	_motors_state.m6 = m6;
+void SimpleCommunicator_t::OnMotorsStateNeed(void(* callback)(void* data, float& m1, float& m2, float& m3, float& m4, float& m5, float& m6), void* data) {
+	_on_motors_state_need.callback = callback;
+	_on_motors_state_need.data = data;
 }
 
-void SimpleCommunicator_t::SetScannedI2CDevices(bool pca1, bool pca2, bool hmc58x3, bool itg3200, bool adxl345, bool bmp085, bool ms5803) {
-	_scanned_i2c_devices.pca1 = pca1;
-	_scanned_i2c_devices.pca2 = pca2;
-	_scanned_i2c_devices.hmc58x3 = hmc58x3;
-	_scanned_i2c_devices.itg3200 = itg3200;
-	_scanned_i2c_devices.adxl345 = adxl345;
-	_scanned_i2c_devices.bmp085 = bmp085;
-	_scanned_i2c_devices.ms5803 = ms5803;
-
-	_last_i2c_scan_token = _last_received_i2c_scan_token;
+void SimpleCommunicator_t::OnScannedI2CDevicesNeed(void(* callback)(void* data, bool& scanned, bool& pca1, bool& pca2, bool& hmc58x3, bool& itg3200, bool& adxl345, bool& bmp085, bool& ms5803), void* data) {
+	_on_scanned_i2c_devices_need.callback = callback;
+	_on_scanned_i2c_devices_need.data = data;
 }
 
 void SimpleCommunicator_t::OnStateReceive(void(* callback)(void* data, bool flashlight_state, bool read_bluetooth, bool send_raw_sensor_data, bool send_calibrated_sensor_data, bool send_pid_state, bool send_motors_state, uint8_t i2c_scan_token), void* data) {
--- MATE_ROV_2017_DNO/SimpleCommunicator.h
+++ MATE_ROV_2017_DNO/SimpleCommunicator.h
@@ -25,7 +25,39 @@ private:
 		void(*callback)(void* data, float depth_p, float depth_i, float depth_d, float yaw_p, float yaw_i, float yaw_d, float pitch_p, float pitch_i, float pitch_d);
 	} _on_pid_receive;
 
-	struct {
+	struct :BaseCallback_t {
+		void(*callback)(void* data, bool& flashlight_state);
+	} _on_state_need;
+
+	struct :BaseCallback_t {
+		void(*callback)(void* data, float& q1, float& q2, float& q3, float& q4, float& depth);
+	} _on_sensor_data_need;
+
+	struct :BaseCallback_t {
+		void(*callback)(void* data, int& ax, int& ay, int& az, int& gx, int& gy, int& gz, int& mx, int& my, int& mz, float& depth);
+	} _on_raw_sensor_data_need;
+
+	struct :BaseCallback_t {
+		void(*callback)(void* data, float& ax, float& ay, float& az, float& gx, float& gy, float& gz, float& mx, float& my, float& mz, float& depth);
+	} _on_calibrated_sensor_data_need;
+
+	struct :BaseCallback_t {
+		void(*callback)(void* data, char*& bluetooth_message);
+	} _on_bluetooth_msg_need;
+
+	struct :BaseCallback_t {
+		void(*callback)(void* data, float& depth_in, float& depth_target, float& depth_out, float& yaw_in, float& yaw_target, float& yaw_out, float& pitch_in, float& pitch_out, float& pitch_target);
+	} _on_pids_state_need;
+
+	struct :BaseCallback_t {
+		void(*callback)(void* data, float& m1, float& m2, float& m3, float& m4, float& m5, float& m6);
+	} _on_motors_state_need;
+
+	struct :BaseCallback_t {
+		void(*callback)(void* data, bool& scanned, bool& pca1, bool& pca2, bool& hmc58x3, bool& itg3200, bool& adxl345, bool& bmp085, bool& ms5803);
+	} _on_scanned_i2c_devices_need;
+
+	struct State_t {
 		int flashlight_state : 1;
 		int read_bluetooth : 1;
 		int send_raw_sensor_data : 1;
@@ -35,95 +67,8 @@ private:
 		int rest : 2;
 	} _all_state;
 
-	struct {
-		int pca1 : 1;
-		int pca2 : 1;
-		int hmc58x3 : 1;
-		int itg3200 : 1;
-		int adxl345 : 1;
-		int bmp085 : 1;
-		int ms5803 : 1;
-	} _scanned_i2c_devices;
-
 	uint8_t _last_i2c_scan_token;
 	uint8_t _last_received_i2c_scan_token;
-
-	struct {
-		float q1;
-		float q2;
-		float q3;
-		float q4;
-		float depth;
-	} _sensor_data;
-
-	struct {
-		int ax;
-		int ay;
-		int az;
-
-		int mx;
-		int my;
-		int mz;
-
-		int gx;
-		int gy;
-		int gz;
-
-		float depth;
-	} _raw_sensor_data;
-
-	struct {
-		float ax;
-		float ay;
-		float az;
-
-		float mx;
-		float my;
-		float mz;
-
-		float gx;
-		float gy;
-		float gz;
-	} _calibrated_sensor_data;
-
-	char _bluetooth_message[7];
-
-	struct Pids_t {
-		float depth_p;
-		float depth_i;
-		float depth_d;
-
-		float yaw_p;
-		float yaw_i;
-		float yaw_d;
-
-		float pitch_p;
-		float pitch_i;
-		float pitch_d;
-	} _pids;
-
-	struct {
-		float m1;
-		float m2;
-		float m3;
-		float m4;
-		float m5;
-		float m6;
-	} _motors_state;
-
-	struct {
-		float depth_in;
-		float depth_target;
-		float depth_out;
-
-		float yaw_in;
-		float yaw_target;
-		float yaw_out;
-
-		float pitch_in;
-		float pitch_target;
-		float pitch_out;
-	} _pids_state;
 
 	uint32_t _pids_hash;
 
@@ -131,6 +76,7 @@ private:
 	unsigned long _last_msg_send_time;
 
 	unsigned int _send_frequency;
+	unsigned int _receive_time_out;
 
 	uint32_t _last_received_msg_id;
 	uint32_t _last_sended_msg_id;
@@ -141,18 +87,18 @@ public:
 	SimpleCommunicator_t(ConnectionProvider_t* connection_provider);
 	void Begin();
 	void Stop();
-	bool Connected();
+	bool Connected() const;
 
 	int Update();
 
-	void SetState(bool flashlight_state);
-	void SetSensorData(float q1, float q2, float q3, float q4, float depth);
-	void SetRawSensorData(int ax, int ay, int az, int gx, int gy, int gz, int mx, int my, int mz, float depth);
-	void SetCalibratedSensorData(float ax, float ay, float az, float gx, float gy, float gz, float mx, float my, float mz);
-	void SetBluetoothMessage(char* bluetooth_message);
-	void SetPids(float depth_p, float depth_i, float depth_d, float yaw_p, float yaw_i, float yaw_d, float pitch_p, float pitch_i, float pitch_d);
-	void SetMotorsState(float m1, float m2, float m3, float m4, float m5, float m6);
-	void SetScannedI2CDevices(bool pca1, bool pca2, bool hmc58x3, bool itg3200, bool adxl345, bool bmp085, bool ms5803);
+	void OnStateNeed(void(*callback)(void* data, bool& flashlight_state), void* data);
+	void OnSensorDataNeed(void(*callback)(void* data, float& q1, float& q2, float& q3, float& q4, float& depth), void* data);
+	void OnRawSensorDataNeed(void(*callback)(void* data, int& ax, int& ay, int& az, int& gx, int& gy, int& gz, int& mx, int& my, int& mz, float& depth), void* data);
+	void OnCalibratedSensorDataNeed(void(*callback)(void* data, float& ax, float& ay, float& az, float& gx, float& gy, float& gz, float& mx, float& my, float& mz, float& depth), void* data);
+	void OnBluetoothMessageNeed(void(*callback)(void* data, char*& bluetooth_message), void* data);
+	void OnPidsStateNeed(void(*callback)(void* data, float& depth_in, float& depth_target, float& depth_out, float& yaw_in, float& yaw_target, float& yaw_out, float& pitch_in, float& pitch_out, float& pitch_target), void* data);
+	void OnMotorsStateNeed(void(*callback)(void* data, float& m1, float& m2, float& m3, float& m4, float& m5, float& m6), void* data);
+	void OnScannedI2CDevicesNeed(void(*callback)(void* data, bool& scanned, bool& pca1, bool& pca2, bool& hmc58x3, bool& itg3200, bool& adxl345, bool& bmp085, bool& ms5803), void* data);
 
 	void OnStateReceive(void(*callback)(void* data, bool flashlight_state, bool read_bluetooth, bool send_raw_sensor_data, bool send_calibrated_sensor_data, bool send_pid_state, bool send_motors_state, uint8_t i2c_scan_token), void* data);
 	void OnDevicesStateReceive(void(*callback)(void* data, float arm_pos, float hand_pos, float m1_pos, float m2_pos, float cam1_pos, float cam2_pos), void* data);
