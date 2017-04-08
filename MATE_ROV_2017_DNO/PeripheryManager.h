622abcf3e948e6e83c4a0d0cc8189c33db32fe7e
J-Veber <kovalsky.corp@gmail.com>
2017-04-08 22:24:11 +1000

    Add BleutoothPeriphery class with ReceiveBluetoothMessage method

--- MATE_ROV_2017_DNO/PeripheryManager.h
+++ MATE_ROV_2017_DNO/PeripheryManager.h
@@ -2,11 +2,14 @@
 
 #include "FlashlightPeriphery.h"
 #include "ManipulatorPeriphery.h"
+#include "BluetoothPeriphery.h"
 
 class PeripheryManager_t {
 private:
 	FlashlightPeriphery_t* _flashlight;
 	ManipulatorPeriphery_t* _manipulator;
+	BluetoothPeriphery_t* _bluetooth;
+
 public:
 	PeripheryManager_t(FlashlightPeriphery_t* flashlight, ManipulatorPeriphery_t* manipulator) {
 		_flashlight = flashlight;
@@ -23,6 +26,11 @@ public:
 
 	void SetFlashlightState(bool state) const {
 		_flashlight->SetState(state);
+	}
+
+	void ReceiveBluetoothMessage(bool state) const
+	{
+		_bluetooth->ReceiveBluetoothMessage();
 	}
 
 	bool GetFlashlightState() const {
