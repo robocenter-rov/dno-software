b482862f0823c652d75b896179459a4efa9d5428
J-Veber <kovalsky.corp@gmail.com>
2017-04-14 19:30:27 +1000

    Manipulator: change ServoMotor into Motor

--- MATE_ROV_2017_DNO/ManipulatorPeriphery.cpp
+++ MATE_ROV_2017_DNO/ManipulatorPeriphery.cpp
@@ -1,6 +1,6 @@
 #include "ManipulatorPeriphery.h"
 
-ManipulatorPeriphery_t::ManipulatorPeriphery_t(PCA9685ServoMotor_t* hand, PCA9685ServoMotor_t* arm) {
+/*ManipulatorPeriphery_t::ManipulatorPeriphery_t(PCA9685ServoMotor_t* hand, PCA9685ServoMotor_t* arm) {
 	_hand = hand;
 	_arm = arm;
 }
@@ -11,4 +11,21 @@ void ManipulatorPeriphery_t::SetHandAngle(float angle_radians) const {
 
 void ManipulatorPeriphery_t::SetArmAngle(float ange_radians) const {
 	_arm->SetAngle(ange_radians);
+}*/
+
+
+ManipulatorPeriphery_t::ManipulatorPeriphery_t(PCA96685Motor_t* hand, PCA96685Motor_t* arm)
+{
+	_hand = hand;
+	_arm = arm;
+}
+
+void ManipulatorPeriphery_t::SetHandThrust(unsigned int thrust) const
+{
+	_hand->SetThrust(thrust);
+}
+
+void ManipulatorPeriphery_t::SetArmThrust(unsigned int thrust) const
+{
+	_arm->SetThrust(thrust);
 }
--- MATE_ROV_2017_DNO/ManipulatorPeriphery.h
+++ MATE_ROV_2017_DNO/ManipulatorPeriphery.h
@@ -1,13 +1,20 @@
 #pragma once
-#include "PCA9685ServoMotor.h"
+//#include "PCA9685ServoMotor.h"
+#include "PCA96685Motor.h"
 
 class ManipulatorPeriphery_t {
 private:
-	PCA9685ServoMotor_t* _hand;
-	PCA9685ServoMotor_t* _arm;
+	PCA96685Motor_t* _hand;
+	PCA96685Motor_t* _arm;
+	/*PCA9685ServoMotor_t* _hand;
+	PCA9685ServoMotor_t* _arm;*/
 public:
-	ManipulatorPeriphery_t(PCA9685ServoMotor_t* hand, PCA9685ServoMotor_t* arm);
+	ManipulatorPeriphery_t(PCA96685Motor_t* hand, PCA96685Motor_t* arm);
+	/*ManipulatorPeriphery_t(PCA9685ServoMotor_t* hand, PCA9685ServoMotor_t* arm);
 
 	void SetHandAngle(float angle_radians) const;
-	void SetArmAngle(float ange_radians) const;
+	void SetArmAngle(float ange_radians) const;*/
+
+	void SetHandThrust(unsigned int thrust) const;
+	void SetArmThrust(unsigned int thrust) const;
 };
