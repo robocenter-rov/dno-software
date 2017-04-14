2f8ab67167dafe57fe24a91fcc5b37fd02454f5c
J-Veber <kovalsky.corp@gmail.com>
2017-04-14 20:50:19 +1000

    add cameras

--- MATE_ROV_2017_DNO/PeripheryManager.h
+++ MATE_ROV_2017_DNO/PeripheryManager.h
@@ -3,17 +3,24 @@
 #include "FlashlightPeriphery.h"
 #include "ManipulatorPeriphery.h"
 #include "BluetoothPeriphery.h"
+#include "PCA9685ServoMotor.h"
 
 class PeripheryManager_t {
 private:
 	FlashlightPeriphery_t* _flashlight;
 	ManipulatorPeriphery_t* _manipulator;
 	BluetoothPeriphery_t* _bluetooth;
+	PCA9685ServoMotor_t* _camera1;
+	PCA9685ServoMotor_t* _camera2;
 
 public:
-	PeripheryManager_t(FlashlightPeriphery_t* flashlight, ManipulatorPeriphery_t* manipulator) {
+	PeripheryManager_t(FlashlightPeriphery_t* flashlight, ManipulatorPeriphery_t* manipulator,
+		BluetoothPeriphery_t* bluetooth, PCA9685ServoMotor_t* camera1, PCA9685ServoMotor_t* camera2) {
 		_flashlight = flashlight;
 		_manipulator = manipulator;
+		_bluetooth = bluetooth;
+		_camera1 = camera1;
+		_camera2 = camera2;
 	}
 
 	void TurnOnFlashlight() const {
@@ -39,5 +46,15 @@ public:
 
 	ManipulatorPeriphery_t* GetManipulator() const {
 		return _manipulator;
+	}
+
+	void SetAngleCamera1(float angle_radians) const
+	{
+		_camera1->SetAngle(angle_radians);
+	}
+
+	void SetAngleCamera2(float angle_radians) const
+	{
+		_camera2->SetAngle(angle_radians);
 	}
 };
