fc47f0d065563e6c4ba2981227802cea3966baf7
Lukaviy <jiukaviy@gmail.com>
2017-04-15 13:28:37 +1000

    MS5803SensorDepth: Add sensor initialization

--- MATE_ROV_2017_DNO/MS5803SensorDepth.cpp
+++ MATE_ROV_2017_DNO/MS5803SensorDepth.cpp
@@ -2,6 +2,9 @@
 
 MS5803SensorDepth_t::MS5803SensorDepth_t(ms5803_addr adress, precision precision) : _sensor(adress) {
 	_precision = precision;
+
+	_sensor.reset();
+	_sensor.begin();
 }
 
 float MS5803SensorDepth_t::GetRawData() {
