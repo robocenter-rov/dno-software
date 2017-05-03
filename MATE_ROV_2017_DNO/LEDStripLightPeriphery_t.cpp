b68b2f3d0cc09bc95d4e748c6b65811353bf451f
J-Veber <kovalsky.corp@gmail.com>
2017-05-03 11:15:19 +1000

    create LEDStripLightPeriphery

---
+++ MATE_ROV_2017_DNO/LEDStripLightPeriphery_t.cpp
@@ -0,0 +1,17 @@
+#include "LEDStripLightPeriphery_t.h"
+#include "Libs/FastLED/FastLED.h"
+
+
+LEDStripLightPeriphery_t::LEDStripLightPeriphery_t()
+{
+}
+
+
+LEDStripLightPeriphery_t::~LEDStripLightPeriphery_t()
+{
+}
+
+void LEDStripLightPeriphery_t::SetColor(uint8_t r, uint8_t g, uint8_t b)
+{
+
+}
---
+++ MATE_ROV_2017_DNO/LEDStripLightPeriphery_t.h
@@ -0,0 +1,9 @@
+#pragma once
+class LEDStripLightPeriphery_t
+{
+public:
+	LEDStripLightPeriphery_t();
+	~LEDStripLightPeriphery_t();
+	void SetColor(uint8_t r, uint8_t g, uint8_t b);
+};
+
