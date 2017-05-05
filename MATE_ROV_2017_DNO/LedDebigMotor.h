b6de9111161703b291dffe7418b3876814e49218
Lukaviy <jiukaviy@gmail.com>
2017-05-06 01:20:35 +1000

    Add LedDebugMotor class

---
+++ MATE_ROV_2017_DNO/LedDebigMotor.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "Arduino.h"
+#include "Motor.h"
+
+class LedDebugMotor_t : public Motor_t {
+private:
+	int _pin;
+public:
+	LedDebugMotor_t(int pin) {
+		_pin = pin;
+		pinMode(pin, OUTPUT);
+	}
+
+	void SetThrust(float thrust) override {
+		analogWrite(_pin, round(constrain(abs(thrust), 0, 1) * 255));
+	}
+};
