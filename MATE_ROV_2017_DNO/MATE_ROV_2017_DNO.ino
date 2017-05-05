3a942bf644ede7c480a989a7a836d2d607e709b7
Lukaviy <jiukaviy@gmail.com>
2017-05-05 17:15:54 +1000

    Update .ino

--- MATE_ROV_2017_DNO/MATE_ROV_2017_DNO.ino
+++ MATE_ROV_2017_DNO/MATE_ROV_2017_DNO.ino
@@ -14,7 +14,10 @@ Main_t* Main;
 void setup() {
 	Exceptions::Init();
 	Wire.begin();
-	Serial1.begin(115200);
+	Serial3.begin(115200);
+
+	pinMode(13, OUTPUT);
+
 #ifdef _DEBUG
 	Serial.begin(9600);
 #endif
@@ -38,16 +41,39 @@ void setup() {
 	SensorRotation_t* rotation_sensor = new SensorRotation_t();
 	MS5803SensorDepth_t* depth_sensor = new MS5803SensorDepth_t(ADDRESS_HIGH, ADC_4096);
 
+	/*float acc_bias[] = { 2.01769, -2.51933, -5.07049 };
+	float acc_scale[] = { 257.626, 257.65, 247.943 };
+	float gyro_bias[] = { 5.60526, 30.625, -17.5811 };
+	float gyro_scale = 1000;*/
+
+	float acc_bias[] = { -6.4322, -6.7379, -28.5567 };
+	float acc_scale[] = { 253.999, 254.304, 249.271 };
+	float gyro_bias[] = { 23.750677, 24.255642, -5.145686 };
+	float gyro_scale = 950;
+
+	float magn_cal_matrix[3][3] =
+	{
+		{ 2.815472, -0.021771, 0.057076 },
+		{ -0.021771, 2.746060, -0.079616 },
+		{ 0.057076, -0.079616, 3.345993 }
+	};
+	float magn_bias[3] = { 97.428119, -39.218888, -21.747817 };
+
+	rotation_sensor->SetCalibrationValues(
+		gyro_bias, gyro_scale,
+		acc_bias, acc_scale,
+		magn_cal_matrix, magn_bias);
+
 	SensorManager_t* sensor_manager = new SensorManager_t(rotation_sensor, depth_sensor);
 
 	MotorsForceDistributor_t* motors_force_distributor = new MotorsForceDistributor_t(sensor_manager, motors);
 
 	Movement_t* movement = new Movement_t(motors, motors_force_distributor, nullptr);
 	
-	FlashlightPeriphery_t* flashlight_periphery = new FlashlightPeriphery_t(13);
-	flashlight_periphery->TurnOn();
+	FlashlightPeriphery_t* flashlight_periphery = new FlashlightPeriphery_t(8);
 	
-	BluetoothPeriphery_t* bluetooth_periphery = new BluetoothPeriphery_t(&Serial2);
+	Serial1.begin(9600);
+	BluetoothPeriphery_t* bluetooth_periphery = new BluetoothPeriphery_t(&Serial1);
 
 	PCA9685ServoMotor_t* camera1_periphery = new PCA9685ServoMotor_t(pwm2, 14);
 	PCA9685ServoMotor_t* camera2_periphery = new PCA9685ServoMotor_t(pwm2, 15);
@@ -65,8 +91,7 @@ void setup() {
 
 	byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
 	//ConnectionProvider_t* connection_provider = new UdpConnectionProvider_t(20, mac, IPAddress(192, 168, 0, 50), 3000);
-	Serial3.begin(115200);
-	ConnectionProvider_t* connection_provider = new UARTConnectionProvider(&Serial1, 300);
+	ConnectionProvider_t* connection_provider = new UARTConnectionProvider(&Serial3, 300);
 	auto communicator = new SimpleCommunicator_t(connection_provider);
 
 	Main = new SimpleMain_t(communicator, movement, sensor_manager, periphery_manager);
@@ -97,6 +122,11 @@ void loop() {
 		}
 		Exceptions::Release();
 #endif
-		while (true) {}
+		while (true) {
+			digitalWrite(13, HIGH);
+			delay(1000);
+			digitalWrite(13, LOW);
+			delay(1000);
+		}
 	}
 }
