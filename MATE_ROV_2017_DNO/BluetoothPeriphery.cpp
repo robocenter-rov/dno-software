c09d6b5f22ef3a52d3e2ca12ee0d6e3ea1967a96
Lukaviy <jiukaviy@gmail.com>
2017-05-05 17:16:14 +1000

    BluetoothPeriphery: Fix bugs

--- MATE_ROV_2017_DNO/BluetoothPeriphery.cpp
+++ MATE_ROV_2017_DNO/BluetoothPeriphery.cpp
@@ -15,17 +15,14 @@ bool BluetoothPeriphery_t::ReceiveBluetoothMessage(char* buff)
 {
 	if (_serial->available()) {
 		char byte;
-		while (_bytes_readed < 7 && (byte = Serial2.read()) > 0) {
-
+		while (_bytes_readed < 7 && (byte = _serial->read()) > 0) {
 			LOG("Byte readed from bluetooth:");
 			LOGLN(byte);
 			_message_buffer[_bytes_readed++] = byte;
-			//buff[_bytes_readed++] = byte;
 		}
-		if (_bytes_readed == 7)
-		{
+		if (_bytes_readed == 7) {
 			memcpy(buff, _message_buffer, sizeof(char) * 7);
-			//buff = _message_buffer;
+			_bytes_readed = 0;
 			return true;
 		}
 	}
