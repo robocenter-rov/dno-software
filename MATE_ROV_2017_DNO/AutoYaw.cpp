28bbe9a1462dc99ac16c3f1e5d65f8adc2ec0b30
Lukaviy <jiukaviy@gmail.com>
2017-05-10 18:49:20 +1000

    Fix auto_yaw

--- MATE_ROV_2017_DNO/AutoYaw.cpp
+++ MATE_ROV_2017_DNO/AutoYaw.cpp
@@ -1,11 +1,10 @@
 #include "AutoYaw.h"
 
 void AutoYaw_t::Update(MotorsForceDistributor_t* motors_force_distributor) {
-	float q0, q1, q2, q3;
-	_sensor_manager->GetRotation(q0, q1, q2, q3);
+	float q1, q2, q3, q4;
+	_sensor_manager->GetRotation(q1, q2, q3, q4);
 
-	//_input = atan2(2 * q1 * q2 - 2 * q0 * q3, 2 * q0 * q0 + 2 * q1 * q1 - 1); Roll
-	_input = -asin(2 * q1 * q3 + 2 * 0 * q2);
+	_input = atan2(2 * q2 * q3 - 2 * q1 * q4, 2 * q1 * q1 + 2 * q2 * q2 - 1);
 
 	_pid.ComputeYaw();
 
