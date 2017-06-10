91e5c10c6a57204ff8783ae7afd380f7e12a8905
J-Veber <kovalsky.corp@gmail.com>
2017-06-11 00:25:41 +1000

    create setColor for movement

--- MATE_ROV_2017_DNO/LEDStripLightPeriphery_t.cpp
+++ MATE_ROV_2017_DNO/LEDStripLightPeriphery_t.cpp
@@ -1,12 +1,29 @@
 #include "LEDStripLightPeriphery_t.h"
-#include "Libs/FastLED/FastLED.h"
+//#include "Libs/FastLED/FastLED.h"
 
 
-LEDStripLightPeriphery_t::LEDStripLightPeriphery_t()
+LEDStripLightPeriphery_t::LEDStripLightPeriphery_t(int dataPin, int ledsNum)
 {
-}
+	_dataPin = dataPin;
+	_ledsNum = ledsNum;
+	_ledStrip = new CRGB[ledsNum];
 
+
+	FastLED.addLeds<WS2811, dataPin, RGB>(_ledStrip, _ledsNum);
+}
 
 LEDStripLightPeriphery_t::~LEDStripLightPeriphery_t()
 {
+}
+
+void LEDStripLightPeriphery_t::SetColor(float thrust_y, float thrust_x, float thrust_z)
+{
+	int y = thrust_y * 127.5 + 127.5;
+	int x = thrust_x  * 127.5 + 127.5;
+	int z = thrust_z  * 127.5 + 127.5;
+	for (int i = 0; i< _ledsNum; i++)
+	{
+		_ledStrip[i] = CRGB(y, x, z);
+	}
+	FastLED.show();
 }
--- MATE_ROV_2017_DNO/LEDStripLightPeriphery_t.h
+++ MATE_ROV_2017_DNO/LEDStripLightPeriphery_t.h
@@ -1,11 +1,17 @@
 #pragma once
-//#include "Libs/FastLED/FastLED.h"
+#include "Libs/FastLED/FastLED.h"
 
 class LEDStripLightPeriphery_t
 {
+private:
+	int _dataPin;
+	int _ledsNum;
+	CRGB* _ledStrip;
+
+
 public:
-	LEDStripLightPeriphery_t();
+	LEDStripLightPeriphery_t(int dataPin, int ledsNum);
 	~LEDStripLightPeriphery_t();
-	//void SetColor(uint8_t r, uint8_t g, uint8_t b);
+	void SetColor(float thrust_y, float thrust_x, float thrust_z);
 };
 
